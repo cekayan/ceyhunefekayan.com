88756475c86b0a104a624b340d50a9785c105c6e
Arda Sevinç <arda@ardasevinc.com>
2025-03-18 18:43:39 +0300

    chore: remove Tailwind CSS dependencies and related configuration from astro.config and package files

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
-import tailwindcss from '@tailwindcss/vite';
-
 import sitemap from '@astrojs/sitemap';
-
 import robotsTxt from 'astro-robots-txt';
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://ceyhunefekayan.com",
-  vite: {
-    plugins: [tailwindcss()]
-  },
-
   integrations: [sitemap(), robotsTxt({sitemap: "https://ceyhunefekayan.com/sitemap-index.xml"})]
 });
