26dd638a022cf870beaf3b2e6c32093b7335fe6e
Arda Sevinç <arda@ardasevinc.com>
2025-03-18 03:20:10 +0300

    feat: add astro-robots-txt integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,13 @@ import tailwindcss from '@tailwindcss/vite';
 
 import sitemap from '@astrojs/sitemap';
 
+import robotsTxt from 'astro-robots-txt';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
 
-  integrations: [sitemap()]
+  integrations: [sitemap(), robotsTxt()]
 });
